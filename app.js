ac6ac97464975e840d26d9b723d2a4db939ae7c2
Krishnansu <21JE0483@iitism.ac.in>
2023-07-14 19:26:37 +0530

    Fixed UI Components of Cart and Homepage. Refactored Code.

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Connect MongoDB Database
+
 mongoose.set("strictQuery",false);
 mongoose.connect("mongodb+srv://Krishnansu:testing123@canteen.u3cbmpx.mongodb.net/userDB",{ useNewUrlParser: true }, function (err) { 
   if (err) throw err;  });
@@ -76,7 +78,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://bewildered-loafers-colt.cyclic.app/auth/google/home",
+    callbackURL: "http://localhost:3000/auth/google/home",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -85,15 +87,15 @@ passport.use(new GoogleStrategy({
       if( !err ){                                                          //Check for any errors
           if( foundUser ){
               exists = true;                                          // Check for if we found any users
-              return cb( null, foundUser );                  //Will return the foundUser
+              return cb( null, foundUser );                           //Will return the foundUser
           }else {
-              // return ["false",profile.id];                                                        //Create a new User
+              // return ["false",profile.id];                         //Create a new User
               const newUser = new User({
                   googleId : profile.id
               });
               newUser.save( function( err ){
                   if(!err){
-                      return cb(null, newUser);                //return newUser
+                      return cb(null, newUser);                        //return newUser
                   }
               });
           }
@@ -101,11 +103,12 @@ passport.use(new GoogleStrategy({
           console.log( err );
       }
     });
-    // User.findOrCreate({ googleId: profile.id}, function (err, user) {
-    //   return cb(err, user);
-    // });
+
   }
 ));
+
+
+//Schemas for different purposes
 
 const menueSchema = {
   name: {
@@ -144,15 +147,21 @@ const Item = mongoose.model("Item",menueSchema);
 const Cart = mongoose.model("Cart",cartSchema);
 const Order = mongoose.model("Order",orderSchema);
 
+
+//Routes
+
+                                         // STUDENT/USER SIDE
+
+//Starting Page
 app.get("/", function(req, res){
   res.render("starting");
 });
 
+//Authentication 
 app.get("/auth/google",
   passport.authenticate('google', { scope: ["profile"] })
   
 );
-
 
 app.get("/auth/google/home",
   passport.authenticate('google', { failureRedirect: "/" }),
@@ -170,21 +179,13 @@ app.get("/auth/google/home",
     
   );
 
-  app.get("/staffLogin",function(req,res){
-    res.render("staffLogin");
-
-  });
-
-  app.get("/staffLogin/err",function(req,res){
-    res.render("staffLoginErr");
-
-  });
-
+//Render Register Page
 app.get("/newUser",function(req,res){
   res.render("newUser");
   
-});
+  });
 
+//Register
 app.post("/newUser",function(req,res){
   const name = req.body.name;
   const mobile = req.body.mobile;
@@ -203,25 +204,9 @@ app.post("/newUser",function(req,res){
   });
 });
 
-app.post("/studentHome",function(req,res){
-  
-  res.redirect("/studentHome");
-  
-
-});
 
 
-
-app.get("/studentHome",function(req,res){
-  
-  
-  Item.find({},function(err,foundItems){
-    res.render("studentHome",{
-      items: foundItems,
-    });
-  })    
-});
-
+//Logout
 app.get("/logout",function(req,res){
 
   req.logout(function(err){
@@ -237,6 +222,199 @@ app.get("/logout",function(req,res){
 });
 
 
+
+
+//Render Student Homepage 
+app.get("/studentHome",function(req,res){
+
+  Item.find({},function(err,foundItems){
+    res.render("studentHome",{
+      items: foundItems,
+    });
+  })    
+});
+
+//Render cart
+app.get("/cart",function(req,res){
+  
+  const id = req.user._id;
+
+  Cart.findOne({Id:id},function(err,foundCart){
+    if(!err){
+      
+     Item.find({},function(err,foundItems){
+      if(!err){
+
+        if(foundCart){
+          res.render("cart2",{foundItems: foundItems,foundCart:foundCart});
+        }
+        else
+        {
+          res.render("cart2",{foundItems: foundItems,foundCart:[]});
+        }
+        
+        
+      }
+     });
+    }
+    
+  });
+ 
+});
+
+
+//Add item to Cart
+app.post("/cart",function(req,res){
+  const stuID = req.user._id;
+
+  const itemID = req.body.addToCart; //
+  
+  Cart.findOne({Id: stuID},function(err,cart){
+    let flag = false;
+    if(!err){
+      if(cart){
+        if(!flag){
+          cart.items.push({itemId: itemID});
+          cart.cart_status = false;
+          cart.save();
+          flag = true;
+        }
+        
+        
+      }else{
+        const newCart = new Cart({
+          Id: stuID,
+          items: [{
+            itemId: itemID,
+          }],
+          cart_status: false
+        });
+        newCart.save();
+      }
+      res.redirect("/studentHome#pricing");
+    }
+  });
+});
+
+//Remove item from cart
+app.post("/cart/delete",function(req,res){
+  const stuID = req.user._id;
+  const itemID = req.body.remove;
+  Cart.findOne({Id:stuID},function(err,cart){
+    
+    cart.items = cart.items.filter(function(item){
+      if(item.itemId != itemID){
+        return item;
+      }
+    }); 
+    cart.save();
+    res.redirect("/cart");
+  });
+});
+
+//Payment Page
+app.get("/placeOrder",function(req,res){
+  const stuID = req.user._id;
+  
+  Cart.findOne({Id: stuID},function(err,foundCart){
+    if(!err){
+          Item.find({},function(err ,foundItem){
+            if(!err){
+              User.find({_id:stuID},function(err ,foundUser){
+                if(!err){
+                  console.log({foundItem: foundItem,foundCart:foundCart,foundUser:foundUser});
+                  res.render("placeOrder",{foundItem: foundItem,foundCart:foundCart,foundUser:foundUser});
+                }
+              
+            });
+            
+    }});
+                
+    }
+  });
+});
+
+
+// For student to place order for items in Cart
+app.post("/placeOrder",function(req,res){   
+  const stuID = req.user._id;
+  console.log("Place order POST");
+  Cart.find({Id:stuID},function(err,foundCart){
+    if(!err)
+    {
+      let ord=[];
+      console.log("Cart: ",foundCart);
+      ord.push(...foundCart[0].items);
+      console.log(ord);
+    
+      
+
+      const newOrder = new Order({
+        Id: foundCart[0].Id,
+        items: ord,
+        completion_status:false,
+        reject_status:false,
+        accept_status:false,
+        orderDate: date
+        
+      });
+      console.log("Created new order");
+      newOrder.save();
+      
+      
+    }
+  });
+  setTimeout(() => {
+    Cart.findOneAndRemove({Id:stuID},function(err,cart){
+      if(err)
+        {
+          console.log(err);
+        }
+        else
+        {
+          console.log("Removed Cart");
+          res.redirect("/orders");
+        }
+  
+    });
+  }, 2000);
+  
+   
+
+        
+});
+
+//Render orders page for student
+app.get("/orders",function(req,res){       
+  const id = req.user._id;
+  Order.find({Id:id},function(err,foundOrders){
+    if(!err)
+    {
+      Item.find({},function(err,foundItems){
+        if(!err)
+        {
+          console.log(foundOrders);
+          res.render("orders",{orders: foundOrders,items:foundItems});
+          
+        }
+      });
+    }
+    
+  });
+
+
+
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                                              //  STAFF/ADMIN SIDE
+
+//Staff Login
 app.post("/login",function(req,res){
   const email= req.body.email;
   const password= req.body.password;
@@ -263,8 +441,18 @@ app.post("/login",function(req,res){
 });
 
 
+//Render StaffLogin Page
+app.get("/staffLogin",function(req,res){
+  res.render("staffLogin");
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+});
+
+app.get("/staffLogin/err",function(req,res){
+  res.render("staffLoginErr");
+
+});
+
+
 app.get("/staffHome",function(req,res){
 
   Item.find({},function(err,foundItems){    
@@ -283,11 +471,12 @@ app.get("/staffHome",function(req,res){
 });
 
 
+//New food item adding form
 app.get("/newItem",function(req,res){
   res.render("newItem");
 });
 
-
+//For Admin to add new item
 app.post("/newItem",function(req,res){
   const itemName = req.body.itemName;
   const price = req.body.price;
@@ -305,18 +494,13 @@ app.post("/newItem",function(req,res){
     newItem.save();
     res.redirect("/staffHome");
   
-
-
 });
 
 
-
-
-
+//Delete Food from menu 
 app.post("/delete",function(req,res){
   
   const checkedItemId = (req.body.deleteItem);
-
 
     Item.findByIdAndRemove(checkedItemId,function(err){
       if(err)
@@ -332,31 +516,23 @@ app.post("/delete",function(req,res){
   
 });
 
+//For admin to view received orders
+app.get("/ordersReceived",function(req,res){         
+  Order.find({},function(err,foundOrders){
+    Item.find({},function(err,foundItems){
+      User.find({},function(err,foundUsers){
+        console.log(foundOrders,foundItems,foundUsers);
+        res.render("ordersReceived",{orders: foundOrders,items: foundItems, users:foundUsers});
 
-app.get("/placeOrder",function(req,res){
-  const stuID = req.user._id;
-  
-  Cart.findOne({Id: stuID},function(err,foundCart){
-    if(!err){
-          Item.find({},function(err ,foundItem){
-            if(!err){
-              User.find({_id:stuID},function(err ,foundUser){
-                if(!err){
-                  res.render("placeOrder",{foundItem: foundItem,foundCart:foundCart,foundUser:foundUser});
-                }
-              
-            });
-            
-    }});
-                
-        
-
-    }
+      })
+    })
+    
   });
+
 });
 
 
-
+//For admin to accept order
 app.post("/staffHome1",function(req,res){
   const orderID = req.body.AcceptStatus;
   console.log(orderID);
@@ -376,22 +552,7 @@ app.post("/staffHome1",function(req,res){
 
 });
 
-app.post("/completion",function(req,res){
-  const OrderID = req.body.success;
-  Order.findOne({_id:OrderID},function(err,order){
-    if(!err)
-    {if(order){
-      order.completion_status=true;
-      
-      order.save();
-      res.redirect("/ordersReceived");
-    }
-      
-    }
-
-});
-})
-
+//For admin to reject order
 app.post("/staffHome0",function(req,res){
   const orderID = req.body.RejectStatus;
   console.log(orderID);
@@ -412,179 +573,29 @@ app.post("/staffHome0",function(req,res){
 
 });
 
-
-
-
-
-
-app.post("/placeOrder",function(req,res){   // ADD ITEMS TO ORDER COLLECTION AND DELETE IT FROM CART
-  const stuID = req.user._id;
-
-  Cart.find({Id:stuID},function(err,foundCart){
+//Mark order as completed
+app.post("/completion",function(req,res){
+  const OrderID = req.body.success;
+  Order.findOne({_id:OrderID},function(err,order){
     if(!err)
-    {
-      let ord=[];
-      console.log(foundCart);
-      ord.push(...foundCart[0].items);
-      console.log(ord);
-    
+    {if(order){
+      order.completion_status=true;
       
-
-      const newOrder = new Order({
-        Id: foundCart[0].Id,
-        items: ord,
-        completion_status:false,
-        reject_status:false,
-        accept_status:false,
-        orderDate: date
-        
-      });
-      newOrder.save();
-      
+      order.save();
+      res.redirect("/ordersReceived");
+    }
       
     }
-  });
-
-  Cart.findOneAndRemove({Id:stuID},function(err,cart){
-    if(err)
-      {
-        console.log(err);
-      }
-      else
-      {
-        console.log("Removed Cart");
-        res.redirect("/orders");
-      }
-
-  });
-   
-
-        
-});
-
-
-app.get("/orders",function(req,res){       
-  const id = req.user._id;
-  Order.find({Id:id},function(err,foundOrders){
-    if(!err)
-    {
-      Item.find({},function(err,foundItems){
-        if(!err)
-        {
-          console.log(foundOrders);
-          res.render("orders",{orders: foundOrders,items:foundItems});
-          
-        }
-      });
-    }
-    
-  });
-
-
 
 });
-
-app.get("/ordersReceived",function(req,res){          //SEND ORDER+USER+ITEM COLLECTION
-  Order.find({},function(err,foundOrders){
-    Item.find({},function(err,foundItems){
-      User.find({},function(err,foundUsers){
-        console.log(foundOrders,foundItems,foundUsers);
-        res.render("ordersReceived",{orders: foundOrders,items: foundItems, users:foundUsers});
-
-      })
-    })
-    
-  });
-
-});
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-app.get("/cart",function(req,res){
-  
-  
-  const id = req.user._id;
-
-  Cart.findOne({Id:id},function(err,foundCart){
-    if(!err){
-      
-     Item.find({},function(err,foundItems){
-      if(!err){
-        // console.log(foundCart);
-        if(foundCart){
-          res.render("cart",{foundItems: foundItems,foundCart:foundCart});
-        }
-        else
-        {
-          res.render("cart",{foundItems: foundItems,foundCart:[]});
-        }
-        
-        
-      }
-     });
-    }
-    
-  });
- 
-  
-
-});
-
-app.post("/cart/delete",function(req,res){
-  const stuID = req.user._id;
-  const itemID = req.body.remove;
-  Cart.findOne({Id:stuID},function(err,cart){
-    
-    cart.items = cart.items.filter(function(item){
-      if(item.itemId != itemID){
-        return item;
-      }
-    }); 
-    cart.save();
-    res.redirect("/cart");
-  });
-});
-
-app.post("/cart",function(req,res){
-  const stuID = req.user._id;
-  console.log(stuID);
-  const itemID = req.body.addToCart; //
-  
-  Cart.findOne({Id: stuID},function(err,cart){
-    let flag = false;
-    if(!err){
-      if(cart){
-        // for(let i =0;i< cart.items.length;i++){
-        //   if(cart.items[i].itemId === itemID){
-        //     flag = true;
-        //     break;
-        //   }
-        // }
-        if(!flag){
-          cart.items.push({itemId: itemID});
-          cart.cart_status = false;
-          cart.save();
-          flag = true;
-        }
-        
-        
-      }else{
-        const newCart = new Cart({
-          Id: stuID,
-          items: [{
-            itemId: itemID,
-          }],
-          cart_status: false
-        });
-        newCart.save();
-      }
-      res.redirect("/studentHome#pricing");
-    }
-  });
-});
+})
 
 
 
+
+
+
+//Start Local Host server
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
