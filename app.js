7c956aa98804145d509448c6ab390e65e9bcbab3
Krishnansu <21JE0483@iitism.ac.in>
2024-01-04 18:56:34 +0530

    Deployment fixes

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/home",
+    callbackURL: "https://delicious-grounds.onrender.com/auth/google/home",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
